8569fdcd33a85e5932770273bccd497a509aa5c1
Jayirong <lemonsoft5555@gmail.com>
2024-03-24 19:17:15 -0300

    first commit

---
+++ src/main/java/com/tienda/tiendita/TienditaApplication.java
@@ -0,0 +1,13 @@
+package com.tienda.tiendita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TienditaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TienditaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tienda/tiendita/TienditaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienda.tiendita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TienditaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
