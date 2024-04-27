e21d8f41d9c84b40f177679417569e73ddbf69b3
Jayirong <lemonsoft5555@gmail.com>
2024-04-27 15:29:07 -0400

    HATEOAS

--- src/main/java/com/tienda/tiendita/Controller/VentaController.java
+++ src/main/java/com/tienda/tiendita/Controller/VentaController.java
@@ -21,8 +21,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-import javax.swing.text.html.parser.Entity;
-
 @RestController
 @RequestMapping("/ventas")
 public class VentaController {
